3f56c846e72ccf175d70d9f74cac409eae61e70f
aryamshabbir <aryamshabbir79@gmail.com>
2025-06-12 09:51:27 -0700

    complete the two functions and add extra test cases

--- lab01/exercise1/test_ex1.c
+++ lab01/exercise1/test_ex1.c
@@ -15,6 +15,9 @@ int main(int argc, char **argv) {
     assert(num_z == 0);
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+    char *empty_str = "";
+    int num_x = num_occurrences(empty_str, 'x');
+    assert(num_x == 0);
 
     printf("Congrats! If you have made it to this line, your Part 1 Test cases are all passing!\n");
 
@@ -36,6 +39,13 @@ int main(int argc, char **argv) {
     assert(dna_seq_2.T_count == 0);
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+    DNA_sequence dna_seq_3;
+    strcpy(dna_seq_3.sequence, "XYZXYZ");
+    compute_nucleotide_occurrences(&dna_seq_3);
+    assert(dna_seq_3.A_count == 0);
+    assert(dna_seq_3.C_count == 0);
+    assert(dna_seq_3.G_count == 0);
+    assert(dna_seq_3.T_count == 0);
 
     printf("Congrats! If you have made it to this line, your Part 2 Test cases are all passing!\n");
 
